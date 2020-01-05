a80a972653cd35933f7aa1bc034301b944358a60
tom-foster <19tfoster87@gmail.com>
2020-01-05 23:17:10 +0000

    initial secure gui commit that is working

---
+++ password_generator_secure_gui.py
@@ -0,0 +1,48 @@
+import string
+import random
+
+from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E
+
+class PassGenGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title('Password Secure Generator')
+
+        self.label = Label(master, text='Enter')
+        self.label.pack()
+
+        vcmd = master.register(self.validate) # we have to wrap the command
+        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+        self.entry.pack()
+
+        self.generate_button = Button(master, text="Generate Password", command=self.gen_password)
+        self.generate_button.pack()
+        # self.close_button = Button(master, text="Close", command=master.quit)
+        # self.close_button.pack()
+
+        
+
+    def password_secure_generator(self, size=20, chars=string.ascii_letters +
+                                string.punctuation + string.digits):
+        return ''.join(random.SystemRandom().choice(chars) for i in range(size))
+
+    def validate(self, new_text):
+        if not new_text: # the field is being cleared
+            self.entered_number = 20
+            return True
+
+        try:
+            self.entered_number = int(new_text)
+            return True
+        except ValueError:
+            return False
+
+    def gen_password(self):
+        self.master.clipboard_clear()
+        self.master.clipboard_append(str(self.password_secure_generator(self.entered_number)))
+
+
+
+root = Tk()
+pass_gen_gui = PassGenGUI(root)
+root.mainloop()
